902462507da56c63a8ef38c67db1939b9dd04f14
pmdartus <dartus.pierremarie@gmail.com>
2013-04-30 18:13:11 +0200

    Revert "Deploy bis"
    This reverts commit ec6f1b4b899e1c4695f12ff89806ea4e7194f02e.

--- Gemfile
+++ Gemfile
@@ -2,11 +2,13 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
 
+gem "heroku"
 group :development, :test do
   gem 'sqlite3'
 end
 group :production do
   gem 'pg'
+  gem 'thin'
 end
 
 # Gems used only for assets and not required
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,9 +1,5 @@
 class Vote < ActiveRecord::Base
   attr_accessible :choice, :email, :tocken, :validated
 
-  validates :email, :uniqueness => true, :email => true
-
-  def validate_email
-    # email_domain = email.domain.to_s
-  end
+  validates :email, :uniqueness => true
 end
