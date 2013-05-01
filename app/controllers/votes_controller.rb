99b40711d1b7e6ca1f58c751615f61ffddf9d9ce
pmdartus <dartus.pierremarie@gmail.com>
2013-05-01 09:08:07 +0200

    generating tocken

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,10 +5,19 @@ class VotesController < ApplicationController
 
   def create
     @vote = Vote.new(params[:vote])
-    if @vote.save
-      redirect_to root_path, notice: 'Confirmation par mail envoye'
+    email = @vote.email
+    if (email.include? "insa-lyon") || (email.include? "insa-strasbourg") || (email.include? "insa-toulouse") || (email.include? "insa-rennes.fr")
+
+      @vote.tocken = unique_identifier = SecureRandom.hex(7)
+      
+      if @vote.save
+        redirect_to root_path, notice: 'Confirmation par mail envoye'
+      else
+        redirect_to root_path, notice: 'Une erreur inatendu est survenue'
+      end
     else
-      redirect_to root_path, notice: 'Une erreur inatendu est survenue'
+      redirect_to root_path, notice: 'Veuillez enter une adresse INSA !'
+    
     end
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,5 @@
 class Vote < ActiveRecord::Base
   attr_accessible :choice, :email, :tocken, :validated
+
+  validates :email, :uniqueness => true
 end
