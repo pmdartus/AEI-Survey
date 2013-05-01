7f6b3b3caafa96a9dd0ae791a91b5168edba5dbd
pmdartus <dartus.pierremarie@gmail.com>
2013-05-01 09:32:21 +0200

    validation ok

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -8,7 +8,12 @@ class VotesController < ApplicationController
     email = @vote.email
     if (email.include? "insa-lyon") || (email.include? "insa-strasbourg") || (email.include? "insa-toulouse") || (email.include? "insa-rennes.fr")
 
-      @vote.tocken = unique_identifier = SecureRandom.hex(7)
+      begin
+        tocken_secret = unique_identifier = SecureRandom.hex(7)
+      end while Vote.exists?(:tocken => tocken_secret)
+
+      @vote.tocken = tocken_secret
+      @vote.validated = false
       
       if @vote.save
         redirect_to root_path, notice: 'Confirmation par mail envoye'
@@ -19,5 +24,24 @@ class VotesController < ApplicationController
       redirect_to root_path, notice: 'Veuillez enter une adresse INSA !'
     
     end
+  end
+
+  def validate
+    tocken = params[:tocken]
+    vote = Vote.find_by_tocken(tocken)
+    if vote
+
+      if !vote.validated
+        vote.validated = true
+        vote.save
+        redirect_to root_path, notice: "Le vote a ete confirme"
+      else
+        redirect_to root_path, notice: "Le vote a deja ete confirme !"
+      end
+
+    else
+      redirect_to root_path, notice: "Confirmation invalide !"
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 AEI::Application.routes.draw do
 
+  match 'votes/validate/:tocken', :controller => 'votes', :action => 'validate'
   resources :votes
   root :to => 'votes#new'
 
